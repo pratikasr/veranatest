fd564f68f0d3fde9dcd5ffde99a2413b071ba587
pratikasr <pratikasr@gmail.com>
2025-08-27 17:21:43 +0530

    feat: added trustDepositShareValue as params
    Signed-off-by: pratikasr <pratikasr@gmail.com>

--- x/td/types/params.go
+++ x/td/types/params.go
@@ -1,17 +1,26 @@
 package types
 
+const DefaultTrustDepositShareValue uint64 = 1
+
 // NewParams creates a new Params instance.
-func NewParams() Params {
-	return Params{}
+func NewParams(trustDepositShareValue uint64) Params {
+	return Params{TrustDepositShareValue: trustDepositShareValue}
 }
 
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DefaultTrustDepositShareValue)
 }
 
 // Validate validates the set of params.
 func (p Params) Validate() error {
+	if err := validateTrustDepositShareValue(p.TrustDepositShareValue); err != nil {
+		return err
+	}
+
+	return nil
+}
+func validateTrustDepositShareValue(v uint64) error {
 
 	return nil
 }
